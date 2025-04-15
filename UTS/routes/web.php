3734471f48084935c199d545ac819cea66e7a127
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2025-04-15 17:45:35 +0700

    ADD UTS

---
+++ UTS/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+
+Route::get('/welcome', function () {
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "2341720148";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
