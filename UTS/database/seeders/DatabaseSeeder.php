ec57896af3ba7a3057728fb9d3003a800fe6286e
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2025-04-20 05:23:06 +0700

    UTS

--- UTS/database/seeders/DatabaseSeeder.php
+++ UTS/database/seeders/DatabaseSeeder.php
@@ -12,10 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            //UkmSeeder::class,
-            //AnggotaSeeder::class,
-            KegiatanSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
